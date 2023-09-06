62db8f7e95b776fa68a8386e71d7795e723b78ed
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2023-09-05 21:06:56 -0300

    feat: search page is now functional
    - add input to search for animals
    - add button to submit search

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,10 +1,95 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useCallback, useRef, useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+
+import {
+  Container,
+  Content,
+  AnimationContainer,
+  Logo,
+  LoadingContainer,
+} from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useToast } from '../../hooks/toast';
+import { useSearch } from '../../hooks/search';
+import { LoadingSpinner } from '../../components/LoadingSpinner';
+import { colors } from '../../styles/colors';
+
+interface SearchFormData {
+  search: string;
+}
 
 function Search(): ReactElement {
+  const formRef = useRef<FormHandles>(null);
+
+  const [searchText, setSearchText] = useState('');
+  const { addToast } = useToast();
+  const { handleSearch, loading } = useSearch();
+  const navigate = useNavigate();
+
+  const handleSubmit = useCallback(
+    async (data: SearchFormData): Promise<void> => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          search: Yup.string().required('Search required'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+        handleSearch(data.search);
+        navigate('/results', { state: { searchText } });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Error in search',
+          description: 'An error occurred in search, try again',
+        });
+      }
+    },
+    [handleSearch, addToast],
+  );
+
   return (
-    <div>
-      <h2>Search Page</h2>
-    </div>
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <Logo src={logoImg} alt="animals_search_logo" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Search for animals</h1>
+            <Input
+              name="search"
+              icon={FiSearch}
+              onChange={e => setSearchText(e.target.value)}
+              iconProps={{ color: colors.greyLight }}
+              showClearButton
+            />
+            <Button type="submit" loading={loading} disabled={!searchText}>
+              Search
+            </Button>
+          </Form>
+        </AnimationContainer>
+        {loading && (
+          <LoadingContainer>
+            <LoadingSpinner />
+          </LoadingContainer>
+        )}
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/pages/Search/styles.ts
@@ -0,0 +1,166 @@
+import styled, { css, keyframes } from 'styled-components';
+import { shade, lighten } from 'polished';
+
+import { colors } from '../../styles/colors';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 95%;
+  max-width: 700px;
+`;
+
+const fromUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-height: 60%;
+  width: 100%;
+  animation: ${fromUp} 1s;
+
+  form {
+    margin: 40px 0;
+    width: 100%;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+`;
+
+const toUp = keyframes`
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(0.6);
+  }
+`;
+
+interface LogoProps {
+  visible?: boolean;
+}
+
+export const Logo = styled.img<LogoProps>`
+  max-height: 30%;
+  animation: ${props => (props.visible ? fromUp : toUp)} 0.5s;
+  ${props =>
+    !props.visible &&
+    css`
+      transform: scale(0.6);
+    `}
+`;
+
+const fromBottom = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const scale = keyframes`
+  from {
+    transform: scale(0);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(1);
+  }
+`;
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 380px;
+  width: 100%;
+  animation: ${scale} 0.6s;
+`;
+
+export const SearchResults = styled.div`
+  width: 100%;
+  animation: ${fromBottom} 1s;
+`;
+
+const changeColor = keyframes`
+  from {
+    transform: translateY(0);
+    background-color: ${colors.primaryLight};
+  }
+  to {
+    transform: translateY(-4px);
+    background-color: ${lighten(0.05, colors.primaryLight)};
+  }
+`;
+
+export const UserFromSearch = styled.div`
+  margin-top: 12px;
+  cursor: pointer;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  background-color: ${colors.primaryLight};
+  border-radius: 10px;
+
+  &:hover {
+    transform: translateY(-4px);
+    animation: ${changeColor} 0.5s;
+    background-color: ${lighten(0.05, colors.primaryLight)};
+  }
+
+  img {
+    height: 50px;
+    border-radius: 25px;
+  }
+
+  span {
+    color: #f4ede8;
+    margin-left: 16px;
+    font-size: 20px;
+    font-weight: bold;
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+  }
+`;
